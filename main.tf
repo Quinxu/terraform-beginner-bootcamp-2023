14bf3689f79e6d2021b285c74304b35cfc36de3c
Qin Xu <quinxu@hotmail.com>
2023-09-26 20:44:19 +0000

    Added aws provider,  created S3 bucket and limited random string to be only lower case letters.

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
@@ -12,13 +16,31 @@ provider "random" {
   # Configuration options
 }
 
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
-  
+  length = 32
+  special = false
+  lower = true
+  upper = false
 }
 
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  #Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
+
+  # tags = {
+  #   Name        = "My bucket"
+  #   Environment = "Dev"
+  # }
 }
