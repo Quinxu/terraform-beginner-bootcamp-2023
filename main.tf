8d6e40e2f330f41537aeb3813c4baa01d05a7372
Qin Xu <quinxu@hotmail.com>
2023-10-03 05:04:21 +0000

    Added files in project root module.

--- main.tf
+++ main.tf
@@ -1,27 +1,9 @@
 
 terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-  cloud {
-    organization = "qinxu"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
+  
+  
 }
 
-provider "random" {
-  # Configuration options
-}
 
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -32,13 +14,8 @@ resource "random_string" "bucket_name" {
 }
 
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
 
-provider "aws" {
-  # Configuration options
-}
+
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
@@ -46,8 +23,7 @@ resource "aws_s3_bucket" "example" {
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
 
-  # tags = {
-  #   Name        = "My bucket"
-  #   Environment = "Dev"
-  # }
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
