4e4d268040c58c47b2c479201af9cdff0ba5258c
Qin Xu <quinxu@hotmail.com>
2023-12-13 20:26:29 +0000

    troubleshoot terraform variables

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -5,3 +5,16 @@ output "bucket_name" {
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
+
+locals {
+  l_error_file_path = var.error_html_file_path
+  l_index_file_path = var.index_html_file_path
+}
+
+output "error_file_path_value" {
+  value = local.l_error_file_path
+}
+
+output "index_file_path_value" {
+  value = local.l_index_file_path
+}
