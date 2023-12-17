824bfa7e6bd642ae798263687820f13fd980450e
Qin Xu <quinxu@hotmail.com>
2023-12-17 15:11:13 +0000

    commented out index and error file related in outpout.

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -6,15 +6,15 @@ output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
 
-locals {
-  l_error_file_path = var.error_html_file_path
-  l_index_file_path = var.index_html_file_path
-}
+# locals {
+#   l_error_file_path = var.error_html_file_path
+#   l_index_file_path = var.index_html_file_path
+# }
 
-output "error_file_path_value" {
-  value = local.l_error_file_path
-}
+# output "error_file_path_value" {
+#   value = local.l_error_file_path
+# }
 
-output "index_file_path_value" {
-  value = local.l_index_file_path
-}
+# output "index_file_path_value" {
+#   value = local.l_index_file_path
+# }
