acd7467e5a6b973de21bc912042a5fdaee05583f
Qin Xu <quinxu@hotmail.com>
2024-02-27 16:55:38 +0000

    updated description and content version. commented out bucket_name variable.

--- variables.tf
+++ variables.tf
@@ -11,10 +11,10 @@ variable "teacherseat_user_uuid" {
   type    = string
 }
 
-variable "bucket_name" {
-#   description = "The name of the S3 bucket"
-  type        = string
-}
+# variable "bucket_name" {
+# #   description = "The name of the S3 bucket"
+#   type        = string
+# }
 
 variable "index_html_file_path" {
   type        = string
