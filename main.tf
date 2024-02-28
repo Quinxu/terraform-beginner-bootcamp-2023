0ab6fe25aee04fbb9ecfc617b6e28ec367cdd92b
Qin Xu <quinxu@hotmail.com>
2024-02-28 17:00:21 +0000

    created seperated folders for two terrahouse and related files.

--- main.tf
+++ main.tf
@@ -60,10 +60,29 @@ resource "terratowns_home" "home" {
   name = "How to carve a pumpkin in 2024!"
   description = <<DESCRIPTION
 It shows the detailed steps to carve a pumpkin.
-# Arcanum is a game from 2001 that shipped with alot of bugs.
-# Modders have removed all the originals making this game really fun
-# to play (despite that old look graphics). This is my guide that will
-# show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  # description = "This is a town located in the west coast. \\nWith population about 10,000."
+  domain_name = module.terrahouse_aws.cloudfront_url
+  # domain_name = "3fdq3gzxq1.cloudfront.net"
+  # town = "gamers-grotto"
+  town = "missingo"
+  content_version = 1
+}
+
+module "terrahouse_aws"{
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  # bucket_name = var.bucket_name
+  index_html_file_path = var.index_html_file_path
+  error_html_file_path = var.error_html_file_path
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
+
+resource "terratowns_home" "home" {
+  name = "How to roast a duck in 2024!"
+  description = <<DESCRIPTION
+It shows the detailed steps to roast a duck.
 DESCRIPTION
   # description = "This is a town located in the west coast. \\nWith population about 10,000."
   domain_name = module.terrahouse_aws.cloudfront_url
